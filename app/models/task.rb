94deed144e9a6a898b2ac12a0aa9e5ee88516469
Gary Wong <garywong79@me.com>
2015-10-19 09:52:45 +0800

    create table, seed data, and interface

---
+++ app/models/task.rb
@@ -0,0 +1,17 @@
+class Task < ActiveRecord::Base
+
+	def list
+		# Task.all.each_with_index do |task,index|
+		# 	puts "#{index+1}. #{task.status} #{task}
+	end
+
+	def add
+	end
+
+	def delete
+	end
+
+	def complete
+	end
+
+end
